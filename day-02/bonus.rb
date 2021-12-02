8996ffd0f91b49e5ba22c49e47efc3b69f4b129c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-12-02 18:53:38 +0000

    Golf the bonus ruby script a bit

--- day-02/bonus.rb
+++ day-02/bonus.rb
@@ -1,6 +1,6 @@
 $depth = 0; $position = 0; $aim = 0;
-def down(n); $aim = $aim + n; end
-def up(n); $aim = $aim - n; end
-def forward(n); $position = $position + n; $depth = $depth + $aim * n; end
-File.readlines('input.txt').each{|l|eval(l)}
+def down(n); $aim += n; end
+def up(n); $aim -= n; end
+def forward(n); $position += n; $depth += $aim * n; end
+eval(File.read('input.txt'))
 puts $depth * $position
