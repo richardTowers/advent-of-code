7b3d767871ecdbe2b1adb94267ed17fef1f50415
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2021-12-02 19:11:52 +0000

    🏌️‍♂️

--- day-02/bonus.rb
+++ day-02/bonus.rb
@@ -2,5 +2,5 @@ $depth = 0; $position = 0; $aim = 0;
 def down(n); $aim += n; end
 def up(n); $aim -= n; end
 def forward(n); $position += n; $depth += $aim * n; end
-eval(File.read('input.txt'))
+load('input.txt')
 puts $depth * $position
