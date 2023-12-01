40a03a6825957e723c6ae97a3f7d5414da501e3b
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2023-12-01 21:03:27 +0000

    2023/01 - reverse reverse reverse

--- 2023/01/solution.rb
+++ 2023/01/solution.rb
@@ -1,40 +1,21 @@
 #!/usr/bin/env ruby
 
-def solve(lines)
+def lookup(haystack, needle)
+  (haystack.index(needle) % 9 + 1).to_s
+end
+
+def solve(lines, patterns)
+  forward_pattern = patterns.join("|")
+  reverse_pattern = patterns.map(&:reverse).join("|")
   values = lines.map do |line|
-    first_digit = line.match(/\d/)[0]
-    last_digit  = line.match(/(\d)\D*$/)[1]
+    first_digit = lookup(patterns, line.match(forward_pattern)[0])
+    last_digit  = lookup(patterns, line.reverse.match(reverse_pattern)[0].reverse)
     first_digit + last_digit
   end
   values.map{|v| v.to_i}.sum
 end
 
-raw_lines = ARGF.readlines()
-
-part_1_result = solve(raw_lines)
-puts "Part 1: #{part_1_result}"
-
-replacements = {
-  "one"   => "one1one",
-  "two"   => "two2two",
-  "three" => "three3three",
-  "four"  => "four4four",
-  "five"  => "five5five",
-  "six"   => "six6six",
-  "seven" => "seven7seven",
-  "eight" => "eight8eight",
-  "nine"  => "nine9nine",
-}
-
-new_lines = raw_lines.map do |line|
-  new_line = line
-  match = nil
-  replacements.each do |word, replacement|
-    new_line.gsub! word, replacement
-  end
-  new_line
-end
-
-part_2_result = solve(new_lines)
-puts "Part 2: #{part_2_result}"
+lines = ARGF.readlines()
+puts solve(lines, ('1'..'9').to_a)
+puts solve(lines, ('1'..'9').to_a + %w{one two three four five six seven eight nine})
 
