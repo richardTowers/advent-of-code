c67f68cb8b45369a289116c78ad3648f42967cff
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2023-12-03 09:44:26 +0000

    2023/03 - ruby - part 2

--- 2023/03/solution.rb
+++ 2023/03/solution.rb
@@ -2,26 +2,34 @@
 
 lines = ARGF.readlines
 grid = lines.map { |l| l.chomp.split("") }
-height = grid.length
-width = grid.first.length
 
-adjacents = grid.map { |row| row.map { |_cell| false } }
-rotations = [-1, 0, 1].product([-1, 0, 1])
-grid.each_with_index do |row, y|
-  row.each_with_index do |cell, x|
-    next unless %w[* - $ % + & / @ = #].include? cell
-
-    rotations.each do |dx, dy|
-      ax = (x + dx).clamp(0..width)
-      ay = (y + dy).clamp(0..height)
-      adjacents[ay][ax] = true
-    end
+ItemArea = Data.define(:value, :x_range, :y_range) do
+  def overlap?(other)
+    x_range.overlap?(other.x_range) && y_range.overlap?(other.y_range)
   end
 end
 
-part_numbers = lines.zip(adjacents).flat_map do |line, adjuncts|
+areas_surrounding_symbols = grid.map.with_index { |row, y|
+  row.map.with_index { |cell, x|
+    ItemArea.new(cell, (x - 1)..(x + 1), (y - 1)..(y + 1)) if "*-$%+&/@=#".include? cell
+  }.compact
+}.flatten(1)
+
+part_numbers = lines.map.with_index { |line, y|
   matches = line.to_enum(:scan, /\d+/).map { Regexp.last_match }
-  matches.select { |m| adjuncts[Range.new(*m.offset(0), true)].any? }.map { |m| m[0].to_i }
+  matches.map { |m| ItemArea.new(m[0].to_i, Range.new(*m.offset(0), true), Range.new(y, y)) }
+}.flatten(1)
+
+valid_part_numbers = part_numbers.select do |p|
+  areas_surrounding_symbols.any? { |area| area.overlap?(p) }
 end
 
-puts "Part 1: #{part_numbers.sum}"
+puts "Part 1: #{valid_part_numbers.sum(&:value)}"
+
+gear_ratios = areas_surrounding_symbols
+  .select { |area| area.value == "*" }
+  .map { |area| part_numbers.select { |p| area.overlap?(p) } }
+  .select { |parts| parts.length == 2 }
+  .map { |parts| parts.map(&:value).inject(:*) }
+
+puts "Part 2: #{gear_ratios.sum}"
