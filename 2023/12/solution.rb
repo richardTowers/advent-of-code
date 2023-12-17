4ae4f291cf9ac5dd5b210acbc5a5ed5bac3b1c76
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2023-12-17 00:18:11 +0000

    2023/5 - ruby - part 2
    Jeesus this was frustrating to debug...

--- 2023/12/solution.rb
+++ 2023/12/solution.rb
@@ -7,17 +7,40 @@ input = ARGF.readlines.map(&:strip).map do |line|
   Row.new(springs, groups)
 end
 
-def permutations(row)
-  if row.springs.include?("?")
-    dotAttempt = Row.new(row.springs.sub("?", "."), row.groups)
-    hashAttempt = Row.new(row.springs.sub("?", "#"), row.groups)
-    permutations(dotAttempt) + permutations(hashAttempt)
-  else
-    groups = row.springs.scan(/#+/).map(&:length)
-    row.groups == groups ? 1 : 0
+@cache = {}
+def solve(row, spring_index = 0, group_index = 0, previous_spring = ".")
+  cache_key = [row, spring_index, group_index, previous_spring]
+  return @cache[cache_key] if @cache.key?(cache_key)
+
+  result = 0
+
+  spring = row.springs[spring_index]
+  group_length = row.groups[group_index]
+  
+  if group_length.nil?
+    if row.springs[spring_index..] =~ /^[?.]*$/
+      return 1 
+    else
+      return 0
+    end
+  elsif spring.nil?
+    return 0
   end
+
+  group_candidate = row.springs[spring_index..][...group_length]
+  following_spring = row.springs[spring_index + group_length]
+
+  result += solve(row, (spring_index + 1), group_index, ".") if spring =~ /[.?]/
+
+  if group_candidate =~ /^[?#]+$/ && following_spring != "#" && previous_spring != "#"
+    result += solve(row, spring_index + group_length, group_index + 1, "#")
+  end
+  
+  @cache[cache_key] = result
+  result
 end
 
-arrangementCounts = input.map.with_index{|row, i| puts i; permutations(row)}
-
+arrangementCounts = input.map{|row| solve(row) }
 puts "Part 1: #{arrangementCounts.sum}"
+foldedCounts = input.map{|row| solve(Row.new((row.springs + "?") * 4 + row.springs, row.groups * 5)) }
+puts "Part 2: #{foldedCounts.sum}"
