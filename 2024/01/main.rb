b0bfa217c1fb9fee63769029c53b16abc9602639
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-01 07:56:27 +0000

    2024/01 :golf:

--- 2024/01/main.rb
+++ 2024/01/main.rb
@@ -1,19 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# Parse input
 lines = ARGF.readlines
-lefts, rights = lines.map(&:split).transpose
-lefts.map!(&:to_i)
-rights.map!(&:to_i)
+lefts, rights = lines.map { _1.split.map(&:to_i) }.transpose
 
 # Part 1
-abs_differences = lefts.sort.zip(rights.sort).map { (_1 - _2).abs }
-puts "Part 1: #{abs_differences.sum}"
+puts "Part 1: #{lefts.sort.zip(rights.sort).map { (_1 - _2).abs }.sum}"
 
 # Part 2
-similarity_score = 0
-lefts.each do |left|
-  count = rights.count { |right| left == right }
-  similarity_score += left * count unless count.zero?
-end
-puts "Part 2: #{similarity_score}"
+tally = rights.tally
+puts "Part 2: #{lefts.map { |l| tally[l]&.then { |c| l * c } }.compact.sum}"
