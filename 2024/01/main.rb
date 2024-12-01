40164576d0967949b53e71065028140a677f925c
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-01 11:27:02 +0000

    2024/01 :golf:

--- 2024/01/main.rb
+++ 2024/01/main.rb
@@ -6,8 +6,10 @@ lines = ARGF.readlines
 lefts, rights = lines.map { _1.split.map(&:to_i) }.transpose
 
 # Part 1
-puts "Part 1: #{lefts.sort.zip(rights.sort).map { (_1 - _2).abs }.sum}"
+part1 = lefts.sort.zip(rights.sort).map { (_1 - _2).abs }.sum
+puts "Part 1: #{part1}"
 
 # Part 2
 tally = rights.tally
-puts "Part 2: #{lefts.map { |l| tally[l]&.then { |c| l * c } }.compact.sum}"
+part2 = lefts.map { |l| tally[l]&.then { |c| l * c } }.compact.sum
+puts "Part 2: #{part2}"
