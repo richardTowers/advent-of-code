aafce55ed8b4763277776f1dd02a0d5b97d38f8a
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-02 10:32:19 +0000

    Add scaffolding for 2024 future days

---
+++ 2024/25/main.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Parse input
+_lines = ARGF.readlines
+
+# Part 1
+part1 = nil
+
+# Part 2
+part2 = nil
+
+# Print output
+puts "Part 1: #{part1}"
+puts "Part 2: #{part2}"
