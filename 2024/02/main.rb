25cdac689f1966e9c354676fecde51b7c3c796a2
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-02 09:30:03 +0000

    2024/02 More :golf:

--- 2024/02/main.rb
+++ 2024/02/main.rb
@@ -10,12 +10,12 @@ def report_safe?(report)
   directions.all? { |d| d == directions.first } && differences.all? { |d| (1..3).include? d }
 end
 
-part1 = reports.select { report_safe?(_1) }.count
+part1 = reports.count { report_safe?(_1) }
 
-part2 = reports.select do |report|
+part2 = reports.count do |report|
   candidate_reports = (0..report.length).map { |index_to_drop| report.reject.with_index { index_to_drop == _2 } }
   [report, *candidate_reports].any? { report_safe?(_1) }
-end.count
+end
 
 puts "Part 1: #{part1}"
 puts "Part 2: #{part2}"
