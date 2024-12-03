9f023578900d0969938a5e88b57349213386bb42
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-03 05:41:08 +0000

    2024/03 :sparkles:
    Very happy with this one. Nice one ruby!

--- 2024/03/main.rb
+++ 2024/03/main.rb
@@ -2,13 +2,34 @@
 # frozen_string_literal: true
 
 # Parse input
-_lines = ARGF.readlines
+input = ARGF.read
+
+Token = Data.define(:op, :l, :r)
+tokens = input
+         .scan(/(\w[\w']+)\((?:(\d+),(\d+))?\)/)
+         .map { |op, l, r| Token.new(op, l.to_i, r.to_i) }
 
 # Part 1
-part1 = nil
+part1 = tokens
+        .select { |token| token.op == 'mul' }
+        .sum { |token| token.l * token.r }
 
 # Part 2
-part2 = nil
+active = true
+part2 = tokens.sum do |token|
+  case token
+  in { op: 'do' }
+    active = true
+    0
+  in { op: "don't" }
+    active = false
+    0
+  in { op: 'mul', l:, r: } if active
+    l * r
+  else
+    0
+  end
+end
 
 # Print output
 puts "Part 1: #{part1}"
