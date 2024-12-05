9aba23a7411c3f287283c6db55c944466302de17
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-05 05:56:12 +0000

    2024/05 part 2

--- 2024/05/main.rb
+++ 2024/05/main.rb
@@ -6,23 +6,42 @@ input = ARGF.read
 
 page_ordering_rules_input, updates_input = input.split("\n\n")
 
-def check_page_ordering_rule(first, second, update)
-  if update.include?(first) && update.include?(second)
-    update.index(first) < update.index(second)
-  else
-    true
+class PageOrderingRule
+  attr_reader :first, :second
+
+  def initialize(first, second)
+    @first = first
+    @second = second
+  end
+
+  def check_page_ordering_rule(update)
+    if update.include?(first) && update.include?(second)
+      update.index(first) < update.index(second)
+    else
+      true
+    end
+  end
+
+  def compare(f, s)
+    if f == first && s == second
+      1
+    elsif f == second && s == first
+      -1
+    else
+      0
+    end
   end
 end
 
 page_ordering_rules = page_ordering_rules_input
                         .split("\n")
                         .map { |rule| rule.split('|') }
-                        .map { |first, second| ->(update) { check_page_ordering_rule(first, second, update) } }
+                        .map { |first, second| PageOrderingRule.new(first, second) }
 
 updates = updates_input.split("\n").map { |update| update.split(',') }
 
 correctly_ordered_updates = updates.select do |update|
-  page_ordering_rules.all? { |rule| rule.call(update) }
+  page_ordering_rules.all? { |rule| rule.check_page_ordering_rule(update) }
 end
 
 puts "#{updates.length} total updates, of which #{correctly_ordered_updates.length} are correct"
@@ -34,8 +53,20 @@ part1 = correctly_ordered_updates.sum do |update|
   update[update.length / 2].to_i
 end
 
+incorrectly_ordered_updates = updates.reject do |update|
+  page_ordering_rules.all? { |rule| rule.check_page_ordering_rule(update) }
+end
+
+incorrectly_ordered_updates.map do |update|
+  update.sort! { |f, s| page_ordering_rules.map { |r| r.compare(f, s) }.reject(&:zero?).first }
+end
+
 # Part 2
-part2 = nil
+part2 = incorrectly_ordered_updates.sum do |update|
+  raise "no middle element" if update.length.even?
+
+  update[update.length / 2].to_i
+end
 
 # Print output
 puts "Part 1: #{part1}"
