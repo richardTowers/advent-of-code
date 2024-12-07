f7600e66ab4240064fa357b97d36fe41df95e91d
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-07 08:02:29 +0000

    2024/0

--- 2024/07/main.rb
+++ 2024/07/main.rb
@@ -1,14 +1,35 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+# Definitions
+Equation = Data.define(:test_value, :numbers) do
+  def valid?(operands)
+    possible_values(operands).include?(test_value)
+  end
+
+  def possible_values(operands)
+    numbers.drop(1).reduce([numbers.first]) do |acc, n|
+      acc.flat_map { operands.call(_1, n) }.select { _1 <= test_value }
+    end
+  end
+end
+
 # Parse input
-_lines = ARGF.readlines
+lines = ARGF.readlines
+equations = lines.map do |line|
+  l, r = line.split(':')
+  Equation.new(l.to_i, r.split.map(&:to_i))
+end
 
 # Part 1
-part1 = nil
+part1 = equations
+        .select { _1.valid?(->(l, r) { [l + r, l * r] }) }
+        .sum(&:test_value)
 
 # Part 2
-part2 = nil
+part2 = equations
+        .select { _1.valid?(->(l, r) { [l + r, l * r, "#{l}#{r}".to_i] }) }
+        .sum(&:test_value)
 
 # Print output
 puts "Part 1: #{part1}"
