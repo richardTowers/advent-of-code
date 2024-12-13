a5e622ca45ec97eab6258101bd100aa98e343b25
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-13 09:37:47 +0000

    2024/13 - rewrite using matrices, part 2

--- 2024/13/main.rb
+++ 2024/13/main.rb
@@ -1,68 +1,36 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# Definitions
-BUTTON_A_COST = 3
-BUTTON_B_COST = 1
-Node = Data.define(:coord, :cost, :parent)
-Machine = Data.define(:button_a, :button_b, :prize)
+require 'matrix'
 
-def build_button(line)
-  button_pattern = /Button .: X\+(?<x>\d+), Y\+(?<y>\d+)/
-  x, y = button_pattern.match(line).values_at(:x, :y).map(&:to_i)
-  x + 1i * y
+PATTERN = /Button A: X\+(\d+), Y\+(\d+)
+Button B: X\+(\d+), Y\+(\d+)
+Prize: X=(\d+), Y=(\d+)/.freeze
+
+input = ARGF.read
+
+equations = input.scan(PATTERN).map do |matching_groups|
+  ax, ay, bx, by, x, y = matching_groups.map(&:to_i)
+  [Matrix[[ax, bx], [ay, by]], Matrix.column_vector([x, y])]
 end
 
-def build_prize(line)
-  prize_pattern = /Prize: X=(?<x>\d+), Y=(?<y>\d+)/
-  x, y = prize_pattern.match(line).values_at(:x, :y).map(&:to_i)
-  x + 1i * y
-end
+def sum_solutions(equations)
+  equations.sum do |button_matrix, prize_vector|
+    solution = button_matrix.inverse * prize_vector
+    a = solution[0, 0]
+    b = solution[1, 0]
+    return 0 unless a.denominator == 1 && b.denominator == 1
 
-def dijkstra(machine)
-  nodes = {}
-  nodes[0] = Node.new(coord: 0, cost: 0, parent: nil)
-
-  until nodes.empty?
-    _, v = nodes.min_by { _2.cost }
-    nodes.delete(v.coord)
-    return v if v.coord == machine.prize
-    next if v.coord.real > machine.prize.real || v.coord.imag > machine.prize.imag
-
-    # TODO: deduplicate this
-    a = v.coord + machine.button_a
-    a_prev_cost = nodes[a]&.cost || Float::INFINITY
-    a_cost = [v.cost + BUTTON_A_COST, a_prev_cost].min
-    a_node = Node.new(coord: a, cost: a_cost, parent: v)
-    nodes[a] = a_node
-
-    b = v.coord + machine.button_b
-    b_prev_cost = nodes[b]&.cost || Float::INFINITY
-    b_cost = [v.cost + BUTTON_B_COST, b_prev_cost].min
-    b_node = Node.new(coord: b, cost: b_cost, parent: v)
-    nodes[b] = b_node
+    (3 * a + b).to_i
   end
-
-  nil
 end
 
-# Parse input
-lines = ARGF.readlines
-machines = lines.each_slice(4).map do |a_line, b_line, prize_line, _|
-  Machine.new(
-    build_button(a_line),
-    build_button(b_line),
-    build_prize(prize_line)
-  )
+part1 = sum_solutions(equations)
+
+part2_equations = equations.map do |button_matrix, prize_vector|
+  [button_matrix, prize_vector.collect { _1 + 10_000_000_000_000 }]
 end
-
-prizes = machines.map.with_index { puts _2; dijkstra(_1) }
-
-# Part 1
-part1 = prizes.compact.sum(&:cost)
-
-# Part 2
-part2 = nil
+part2 = sum_solutions(part2_equations)
 
 # Print output
 puts "Part 1: #{part1}"
