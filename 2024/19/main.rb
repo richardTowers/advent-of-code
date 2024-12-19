38c6698338d7c3e4c4dc02cfee03726e5f20f0b6
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-19 06:14:56 +0000

    2024/19 :sparkles:

--- 2024/19/main.rb
+++ 2024/19/main.rb
@@ -1,14 +1,30 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+$memo = {}
+def count_arrangements(target, patterns)
+  $memo[target] ||=
+    if target.empty?
+      1
+    else
+      patterns.select { target.start_with?(_1) }.sum do |prefix|
+        count_arrangements(target.sub(/^#{prefix}/, ''), patterns)
+      end
+    end
+end
+
 # Parse input
-_lines = ARGF.readlines
+lines = ARGF.readlines.map(&:chomp)
+
+available_patterns = lines[0].split(', ').to_set
+desired_results = lines[2..]
 
 # Part 1
-part1 = nil
+counts = desired_results.map { count_arrangements(_1, available_patterns) }
+part1 = counts.count(&:positive?)
 
 # Part 2
-part2 = nil
+part2 = counts.sum
 
 # Print output
 puts "Part 1: #{part1}"
