ad076ae93b351da6ce9d587939ca93e0d669f0a9
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-24 08:03:42 +0000

    2024/24 part 1 :santa:

--- 2024/24/main.rb
+++ 2024/24/main.rb
@@ -2,10 +2,29 @@
 # frozen_string_literal: true
 
 # Parse input
-_lines = ARGF.readlines
+input = ARGF.read
+wires_input, operations_input = input.split("\n\n")
+wires = wires_input.split("\n")
+                   .to_h { /(\w\d\d): ([10])/.match(_1)[1..] }
+                   .transform_values(&:to_i)
+operations = operations_input.split("\n")
+                             .map { /(\w+) (\w+) (\w+) -> (\w+)/.match(_1)[1..] }
+
+op_queue = operations.dup
+until op_queue.empty?
+  op = op_queue.find { |l, _, r, _| wires.key?(l) && wires.key?(r) }
+  op_queue.delete(op)
+  left, operator, right, result = op
+  wires[result] =
+    case operator
+    when 'AND' then wires.fetch(left) & wires.fetch(right)
+    when 'OR' then wires.fetch(left) | wires.fetch(right)
+    when 'XOR' then wires.fetch(left) ^ wires.fetch(right)
+    end
+end
 
 # Part 1
-part1 = nil
+part1 = wires.select { _1 =~ /z\d\d/ }.sort.reverse.map { _2 }.join('').to_i(2)
 
 # Part 2
 part2 = nil
