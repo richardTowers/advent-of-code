153c73652a1ce8c43ce528841d53a3a91e72357e
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2024-12-24 07:31:29 +0000

    2024/21 WIP

--- 2024/21/main.rb
+++ 2024/21/main.rb
@@ -1,69 +1,88 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'debug'
-
-def breadth_first_traversal(keypad, start)
-  parents = { start => nil }
-  queue = [start]
-  until queue.empty?
-    v = queue.shift
-    [1 + 0i, -1 + 0i, 0 + 1i, 0 - 1i]
-      .map { v + _1 }
-      .select { |coord| keypad.include?(coord) }
-      .reject { |coord| parents.key?(coord) }
-      .each { |coord| queue << coord }
-      .each { |coord| parents[coord] = v }
-  end
-  parents
-end
-
-def path(start, target, parents)
-  result = [target]
-  until target == start
-    target = parents[target]
-    result << target
-  end
-  result.reverse
-end
-
-numeric_keypad = {
-  '7' => 0 + 0i, '8' => 1 + 0i, '9' => 2 + 0i,
-  '4' => 0 + 1i, '5' => 1 + 1i, '6' => 2 + 1i,
-  '1' => 0 + 2i, '2' => 1 + 2i, '3' => 2 + 2i,
-  '0' => 1 + 3i, 'A' => 2 + 3i,
+# Data from the puzzle
+numeric = [
+  %w[7 8 9],
+  %w[4 5 6],
+  %w[1 2 3],
+  %w[# 0 A],
+]
+directional = [
+  %w[# ^ A],
+  %w[< v >],
+]
+directions = {
+  0 + 1i => 'v',
+  0 - 1i => '^',
+  1 + 0i => '>',
+  -1 + 0i => '<',
 }
-directional_keypad = {
-  '^' => 1 + 0i, 'A' => 2 + 0i,
-  '<' => 0 + 1i, 'v' => 1 + 1i, '>' => 2 + 1i
-}
-directions = { 1 + 0i => '>', -1 + 0i => '<', 0 + 1i => 'v', 0 - 1i => '^' }
 
-paths = {}
-numeric_keypad.each do |start_key, start_coord|
-  parents = breadth_first_traversal(numeric_keypad.values.to_set, start_coord)
-  numeric_keypad.each do |end_key, end_coord|
-    path = path(start_coord, end_coord, parents)
-    path_dirs = path.each_cons(2).map { _2 - _1 }
-    paths[[start_key, end_key]] = path_dirs.map { directions[_1] }
+def build_grid(arr)
+  arr.flat_map.with_index do |row, ri|
+    row.map.with_index { |cell, ci| [ci + 1i * ri, cell] }.reject { _2 == '#' }
+  end.to_h
+end
+
+def find_paths(grid, directions)
+  grid.keys.product(grid.keys).to_h do |from, to|
+    vector = to - from
+    reals = (vector.real.positive? ? [1 + 0i] : [-1 + 0i]) * vector.real.abs
+    imags = (vector.imag.positive? ? [0 + 1i] : [0 - 1i]) * vector.imag.abs
+    paths = (reals + imags).permutation.select do |moves|
+      f = from
+      moves.map { |d| f += d }.all? { grid.key?(_1) }
+    end.uniq
+    [[grid[from], grid[to]], paths.map { |path| path.map { directions[_1] } }]
   end
 end
 
-numeric_sequence1 = %w[0 2 9 A]
-direction_sequence1 = (['A'] + numeric_sequence1).each_cons(2).map { |from, to| paths[[from, to]].join('') }.join('A')
-pp [numeric_sequence1, direction_sequence1]
-# move from A to 0
-# move from 0 to A
-# push A
-# move from A to 2
-# move from 2 to A
-# push A
-# move from A to 9
-# move from 9 at A
-# push A
+num_grid = build_grid(numeric)
+num_paths = find_paths(num_grid, directions)
+# pp num_paths.map { |ft, paths| [*ft, paths.map { _1.join('') }] }
+
+dir_grid = build_grid(directional)
+
+dir_paths = find_paths(dir_grid, directions)
+grids = [num_paths, dir_paths, dir_paths]
+# pp dir_paths.map { |ft, paths| [*ft, paths.map { _1.join('') }] }
+
+def enter(row, grids, depth = 0)
+  (['A'] + row).each_cons(2).map do |pair|
+    if depth < grids.length
+      ng = grids[depth][pair].map { _1 + ['A'] }
+      ng.flat_map { { parent: pair, self: _1, depth:, paths: enter(_1, grids, depth + 1) } }
+    else
+      row
+    end
+  end
+end
+
+# def enter_directions(directions, dir_paths)
+#   (['A'] + directions).each_cons(2).map do |pair|
+#     dir_paths[pair].map { _1 + ['A'] }
+#   end
+# end
+#
+# def enter_row(row, num_paths, dir_paths)
+#   (['A'] + row).each_cons(2).map do |nb1, nb2|
+#     nps = num_paths[[nb1, nb2]].map { _1 + ['A'] }
+#     pp ['nps', nps]
+#     dp1s = nps.flat_map { |np| enter_directions(np, dir_paths) }
+#     pp ['dp1s', dp1s]
+#     dp2s = dp1s.flat_map { |dp1| dp1.flat_map { |dp| enter_directions(dp, dir_paths) } }
+#     pp ['dp2s', dp2s]
+#     dp2s.min_by(&:length)
+#   end
+# end
+
+pp enter(%w[0 2 9 A], [num_paths, dir_paths, dir_paths])
 
 # Parse input
-_lines = ARGF.readlines
+lines = ARGF.readlines
+rows = lines.map { _1.chomp.split('') }
+
 
 # Part 1
 part1 = nil
