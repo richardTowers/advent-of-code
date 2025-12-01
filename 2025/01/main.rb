29c21ad9227249e3a2dfe9c526d272bab1b8b170
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-12-01 09:43:36 +0000

    2025/02 Part 2 - ugly version

--- 2025/01/main.rb
+++ 2025/01/main.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
-input = ARGF.readlines
-  .map { /(L|R)(\d+)/.match(it) }
-  .map { |match| [match[1], match[2].to_i] }
+input = ARGF.readlines.map { it.sub(?L, ?-).sub(?R, ?+) }.map(&:to_i)
 
+zeros = 0
 dial = 50
-positions = input.map do |dir, n|
-  case dir
-  in "L" then dial = (dial - n)
-  in "R" then dial = (dial + n)
-  end
+positions = input.map do |n|
+  new_dial = dial + n
+  range = Range.new(*[dial + (n <=> 0), new_dial].sort)
+  zeros += range.map { it % 100 }.count(&:zero?)
+  dial = new_dial
 end
 
 puts "Part 1: #{positions.count { (it % 100).zero? }}"
+puts "Part 2: #{zeros}"
 
 
