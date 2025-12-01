8498a99e96c6f03483cdfabc302e03b9ebe5f944
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-12-01 10:19:45 +0000

    2025/02 :golf:

--- 2025/01/main.rb
+++ 2025/01/main.rb
@@ -1,17 +1,12 @@
 #!/usr/bin/env ruby
 
 input = ARGF.readlines.map { it.sub(?L, ?-).sub(?R, ?+) }.map(&:to_i)
+positions = input.reduce([50]) { |acc, n| acc << acc.last + n }
 
-zeros = 0
-dial = 50
-positions = input.map do |n|
-  new_dial = dial + n
-  range = Range.new(*[dial + (n <=> 0), new_dial].sort)
-  zeros += range.map { it % 100 }.count(&:zero?)
-  dial = new_dial
-end
-
-puts "Part 1: #{positions.count { (it % 100).zero? }}"
-puts "Part 2: #{zeros}"
-
+pp [
+  positions.count { (it % 100).zero? },
+  positions
+    .each_cons(2)
+    .sum { |l, r| Range.new(*[l+(r<=>l),r].sort).count { (it % 100).zero? } }
+]
 
