ca7a4169e22a4e7c3f16f4e91203cc6dcf8c08f9
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-12-03 07:07:52 +0000

    Light :golf:

--- 2025/03/main.rb
+++ 2025/03/main.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env ruby
 
-banks = ARGF.readlines.map(&:chomp).map { it.split("") }
+banks = ARGF.readlines.map(&:chomp)
 
 def largest_joltage(bank, battery_count)
   return "" if battery_count < 1
-  max = bank[..-battery_count].max
+  max = bank[..-battery_count].chars.max
   index = bank.index(max)
   max + largest_joltage(bank[index+1..], battery_count - 1)
 end
