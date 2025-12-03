9cd0f52db6360a6a1ed6de0bc802ec9cef075a13
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-12-03 07:05:42 +0000

    2025/03 part 2

--- 2025/03/main.rb
+++ 2025/03/main.rb
@@ -2,10 +2,15 @@
 
 banks = ARGF.readlines.map(&:chomp).map { it.split("") }
 
-pp (banks.sum do |bank|
-  lmax = bank[...-1].max
-  lindex = bank.index(lmax)
-  rmax = bank[lindex+1..].max
-  (lmax + rmax).to_i
-end)
+def largest_joltage(bank, battery_count)
+  return "" if battery_count < 1
+  max = bank[..-battery_count].max
+  index = bank.index(max)
+  max + largest_joltage(bank[index+1..], battery_count - 1)
+end
 
+
+pp [
+  banks.map { largest_joltage(it,  2) }.sum(&:to_i),
+  banks.map { largest_joltage(it, 12) }.sum(&:to_i),
+]
