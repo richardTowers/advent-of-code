55193c1024306e7f46a66c5bce23fefc9c1a3f59
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2025-12-11 09:14:51 +0000

    2025/11 - :golf:

--- 2025/11/main.rb
+++ 2025/11/main.rb
@@ -1,25 +1,18 @@
 #!/usr/bin/env ruby
 
-input = $<.map { it.split(?:) }.to_h { [_1, _2.strip.split(" ")] }
+GRAPH = Hash.new { [] }
+$<.map { it.split(?:) }.each { GRAPH[_1] = _2.strip.split(" ") }
 
 CACHE = {}
-def dfs(start, targets, &block)
-  target, *remainder = targets
-  key = [start, targets]
-  return CACHE[key] if CACHE.key?(key)
-  if start == target
-    return 1 if remainder.empty?
-    return dfs(target, remainder, &block)
-  end
+def dfs(start, targets)
+  current_target, *remainder = targets
+  return remainder.empty? ? 1 : dfs(current_target, remainder) if start == current_target
 
-  ids = block.call(start)
-  CACHE[key] = ids.nil? ? 0 : ids.sum { dfs(it, targets, &block) }
+  CACHE[[start, targets]] ||= GRAPH[start].sum { |node| dfs(node, targets) }
 end
 
-part_1 = dfs("you", ["out"]) { input[it] }
-a = dfs("svr", ["fft", "dac", "out"]) { input[it] }
-b = dfs("svr", ["dac", "fft", "out"]) { input[it] }
-part_2 = a + b
+part_1 = dfs("you", ["out"])
+part_2 = [["fft", "dac", "out"],["dac", "fft", "out"]].sum { |path| dfs("svr", path) }
 
 pp [
   part_1,
